83cc65b017d8685a7ece9d2ac3601fec641ba0e5
dante ccarhuas <ccarhuas.dante@gmail.com>
2016-04-11 00:57:57 -0500

    Clase Creada

---
+++ WebApplication1/WebApplication1/Models/EmpleadoBean.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EmpleadoBean
+    {
+        public int codigo { get; set; }
+        public string nombres { get; set; }
+        public string direccion { get; set; }
+    }
+}
